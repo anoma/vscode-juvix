d9f725387995867fe05f2f7cc1674734a7cb44eb
Jonathan Cubides <jonathan.cubides@uib.no>
2022-08-10 13:26:20 +0200

    Add juvix version to the status bar and command getBinVersion

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,53 @@
 import * as vscode from 'vscode';
 
+let juvixStatusBarItem : vscode.StatusBarItem;
+
 export function activate(context: vscode.ExtensionContext) {
 
-    console.log('Juvix Extension is now active!');
-    
-// begin example of a modal:
-    const msg = vscode.commands.registerCommand(
-        'juvix.typecheck', () => {
-        vscode.window.showInformationMessage('No type checking errors', { modal: false });
-        // vscode.ShellExecution()
-    });
-    context.subscriptions.push(msg);
-// end of the example
-    // const activatePS = require('./Main').main;
-}
+    // let's put the version in the status bar
+    juvixStatusBarItem = vscode.window.createStatusBarItem
+        (vscode.StatusBarAlignment.Left);
+    context.subscriptions.push(juvixStatusBarItem);
 
-export function deactivate() {}
+    const { spawnSync } = require('child_process');
+    const ls = spawnSync('juvix', ['--version']);
+    let execJuvixVersion : string;
+    if (ls.status !== 0) {
+        vscode.window.showInformationMessage('Juvix binary not found')
+        execJuvixVersion = ls.stderr.toString();
+        juvixStatusBarItem.text = 'Juvix not found';
+    } else {
+    execJuvixVersion = ls.stdout.toString();
+    const juvixBinaryVersion : string = execJuvixVersion.split('\n')[0];
+    juvixStatusBarItem.text = '🛠️ ' + juvixBinaryVersion;
+    }
+    context.subscriptions.push(vscode.commands.registerCommand(
+        'juvix.getBinaryVersion', () => {
+            vscode.window.showInformationMessage(execJuvixVersion
+                , {modal : true});
+            }
+    ));
+    juvixStatusBarItem.show();
+
+    
+    // Show a message telling if the user has already installed 
+    // the Juvix binary or not.
+
+    // context.subscriptions.push(vscode.commands.registerCommand(
+    //     'juvix.getBinaryVersion', () => {
+           
+    //         vscode.window.showInformationMessage(juvixBinaryVersion, { modal: false });
+    //     }
+
+    // ));
+
+    // // begin example of a modal:
+    // const msg = vscode.commands.registerCommand(
+    //     'juvix.typecheck', () => {
+    //         vscode.window.showInformationMessage('Siii type checking errors', { modal: false });
+    //         // vscode.ShellExecution()
+    //     });
+    // context.subscriptions.push(msg);
+};
+
+export function deactivate() { }
