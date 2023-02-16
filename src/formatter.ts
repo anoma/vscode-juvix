96e1ddeea22889ee85cb1db063d6aa19ae223196
Jonathan Cubides <jonathan.cubides@uib.no>
2023-02-16 14:36:28 +0100

    Remove loadRepl keybinding and fix JuvixGeb language

--- src/formatter.ts
+++ src/formatter.ts
@@ -9,6 +9,8 @@ import { debugChannel } from './utils/debug';
 export function activate(_context: vscode.ExtensionContext) {
   const config = new JuvixConfig();
 
+  // TODO: factorize all the following provides, the share
+  // the body except by the body.
   vscode.languages.registerDocumentFormattingEditProvider('Juvix', {
     provideDocumentFormattingEdits(
       document: vscode.TextDocument
@@ -28,6 +30,94 @@ export function activate(_context: vscode.ExtensionContext) {
         'scope',
         filePath,
         '--with-comments',
+      ].join(' ');
+
+      debugChannel.appendLine(formatterCall);
+
+      const { spawnSync } = require('child_process');
+      const ls = spawnSync(formatterCall, {
+        shell: true,
+        input: document.getText(),
+        encoding: 'utf8',
+      });
+
+      debugChannel.appendLine(ls.stdout);
+
+      if (ls.status == 0) {
+        const stdout = ls.stdout;
+        return [vscode.TextEdit.replace(range, stdout)];
+      } else {
+        const errMsg: string = ls.stderr.toString();
+        throw new Error(errMsg);
+      }
+    },
+  });
+
+  // JuvixCore
+
+  vscode.languages.registerDocumentFormattingEditProvider('JuvixCore', {
+    provideDocumentFormattingEdits(
+      document: vscode.TextDocument
+    ): vscode.TextEdit[] {
+      const range = new vscode.Range(
+        document.positionAt(0),
+        document.positionAt(document.getText().length)
+      );
+
+      const filePath = document.uri.fsPath;
+      debugChannel.appendLine(`Formatting ${filePath}`);
+      const formatterCall = [
+        config.getJuvixExec(),
+        config.getGlobalFlags(),
+        '--stdin',
+        'dev',
+        'core',
+        'read',
+        filePath,
+      ].join(' ');
+
+      debugChannel.appendLine(formatterCall);
+
+      const { spawnSync } = require('child_process');
+      const ls = spawnSync(formatterCall, {
+        shell: true,
+        input: document.getText(),
+        encoding: 'utf8',
+      });
+
+      debugChannel.appendLine(ls.stdout);
+
+      if (ls.status == 0) {
+        const stdout = ls.stdout;
+        return [vscode.TextEdit.replace(range, stdout)];
+      } else {
+        const errMsg: string = ls.stderr.toString();
+        throw new Error(errMsg);
+      }
+    },
+  });
+
+  // JuvixGeb
+
+  vscode.languages.registerDocumentFormattingEditProvider('JuvixGeb', {
+    provideDocumentFormattingEdits(
+      document: vscode.TextDocument
+    ): vscode.TextEdit[] {
+      const range = new vscode.Range(
+        document.positionAt(0),
+        document.positionAt(document.getText().length)
+      );
+
+      const filePath = document.uri.fsPath;
+      debugChannel.appendLine(`Formatting ${filePath}`);
+      const formatterCall = [
+        config.getJuvixExec(),
+        config.getGlobalFlags(),
+        '--stdin',
+        'dev',
+        'geb',
+        'read',
+        filePath,
       ].join(' ');
 
       debugChannel.appendLine(formatterCall);
